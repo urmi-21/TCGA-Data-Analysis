2fa1666f14f2af739d95649afe131a0cbdb30879
urmi-21 <mrbains21@gmail.com>
2019-05-02 22:14:59 -0500

    Update getmaf.R

--- getmaf.R
+++ getmaf.R
@@ -1,20 +1,17 @@
-#process mutation data
+#R script process mutation data
 library(dplyr)
-library(DT)
-library(plyr)
-library(data.table)
 library(maftools)
-library("readr")
 library(TCGAbiolinks)
-library(ggplot2)
-packageVersion("TCGAbiolinks")
 
-#download and combine maf files
+
+
+#function to download maf file given a project name
 getmaf<-function(projName){
   thisMaf<-GDCquery_Maf(projName, pipelines = "varscan2")
   return(thisMaf)
 }
 
+#function to download maf files given a list project names. Returned files is combined into one bigger file.
 getmafs<-function(projList){
   allMaf<-data.frame()
   for(p in projList){
@@ -24,17 +21,19 @@ getmafs<-function(projList){
   return(allMaf)
 }
 
+#function to extract transcript information from maf file
 geneToTranscript<-function(mafFile){
   return(distinct(mafFile[,c("Transcript_ID","Gene","Hugo_Symbol","ENSP","SWISSPROT","TREMBL","BIOTYPE","RefSeq")]))
 }
 
-############################################################################
+###################################Example usage############################
 tcgamafProjList<-c("BLCA","BRCA","CESC","UCEC","UCS","READ","COAD","LIHC","HNSC","ESCA","PRAD","STAD","THCA","LUAD","LUSC","KIRC","KIRP","KICH")
 ucsmaf<-getmaf("UCS")
 ucecmaf<-getmaf("UCEC")
 uvmmaf<-getmaf("UVM")
 mafs<-getmafs(c("UCS","UVM","BLCA"))
 
+#read maf file with read.maf function
 maf<-read.maf(mafs,isTCGA = T)
 plotmafSummary(maf = maf)
 
